c091ee519dac43a7dcdef8f1ce1283bdc0a0e9b4
And4Web <anands2807@gmail.com>
2022-05-20 12:54:04 +0530

    update accoutn section created in the body

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -5,11 +5,12 @@ function Body() {
   return (
     <div className="body">
       <div className="update-message">
+        <h2>Update Your Account</h2>
         <p>
           Deleting account cannot be undone <strong>Anand</strong>! You should
           confirm your password to delete your account.
         </p>
-        <span>Delete Account</span>
+        <button>Delete Account</button>
       </div>
     </div>
   );
